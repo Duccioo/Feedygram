175552467cf0872b68fafd75ab52efe899e001ba
Duccioo <86722319+Duccioo@users.noreply.github.com>
2022-10-03 17:19:19 +0200

    Update other_commands.py

--- src/command/other_commands.py
+++ src/command/other_commands.py
@@ -42,11 +42,11 @@ def list_handler(db_telegraph, alias, url, index):
 def help_message():
     return (
         "Need help? No Problem!!\n<b>QUICK START:</b>\n\n"
-        + "*️⃣For add new RSS url type: <code>/add *your_RSS_url*</code>\n<i>(optional)</i> if you want to give a name for that entry try: <code>/add *your_RSS_url* *name_of_entry*</code>\n\n"
-        + "*️⃣For remove a feed type <code>/remove</code> and then click the target feed\n\n"
-        + "*️⃣For change the default link type try <code>/list</code> then click the button below of your target link\n\n"
-        + "*️⃣For get an instant article try to type <code>/get</code> then select your feed and then select how many articles do you want to receive!\n\n"
-        + "*️⃣If you want to know more about this bot try <code>/about</code>"
+        + "*️⃣For add new RSS url type: <b>/add *your_RSS_url*</b>\n<i>(optional)</i> if you want to give a name for that entry try: <b>/add *your_RSS_url* *name_of_entry*</b>\n\n"
+        + "*️⃣For remove a feed type <b>/remove</b> and then click the target feed\n\n"
+        + "*️⃣For change the default link type try <b>/list</b> then click the button below of your target link\n\n"
+        + "*️⃣For get an instant article try to type <b>/get</b> then select your feed and then select how many articles do you want to receive!\n\n"
+        + "*️⃣If you want to know more about this bot try <b>/about</b>"
     )
 
 
@@ -57,15 +57,15 @@ def stop_handler(db, telegram_user):
 
 def about_message(number):
     message = (
-        "Hi🙃! Hope you are finding this bot useful, if so then spread the word and tell your friends about <a href='https://t.me/feedygram_bot'>🐕Feedygram</a>!!\n"
-        + "For more info check the github page: <a href='https://github.com/Duccioo/Feedygram'>Feedygram</a>\n"
+        "Hi🙃! Hope you are finding this bot useful,\nif so then spread the word and tell your friends about <a href='https://t.me/feedygram_bot'>🐕Feedygram</a>!!\n"
+        + "For more info check the  <a href='https://github.com/Duccioo/Feedygram'>github page.</a>\n"
        
         "\nThis bot was made with passion by "
         + random_emoji()
-        + "Duccio Meconcelli (@Dosium).\n\nBased on the <a href='https://github.com/hamitdurmus/robotrss'>RobotRSS by hamitdurmus. </a>\n\n"
+        + "Duccio Meconcelli (@Dosium).\n\nBased on the <a href='https://github.com/hamitdurmus/robotrss'>RobotRSS by hamitdurmus. </a>\n"
         + "The telegraph <a href='https://github.com/NullPointerMaker/webpage2telegraph'>webpage converter library</a> was made by <a href='https://github.com/NullPointerMaker'>NullPointerMaker</a> \n\n"
         + "For other stuff I made check my GitHub: <a href='https://duccio.me/'>Duccioo</a>\n"
-        +"And if you have any feedback, please reach out to me at meconcelliduccio@gmail.com or visit my website <a href='https://duccio.me/'>duccio.me</a>"
+        +"And if you have any feedback, please reach out to me at meconcelliduccio@gmail.com or visit my website <a href='https://duccio.me/'>duccio.me</a>\n"
         + "<i>🐶Bau Bau🐶</i>"
         
          + "\n\nThere are currently "
